5aff774190faa56c64fdf49fc67b0805e8f5ee53
StimUL <stimul881988@mail.ru>
2024-03-17 22:19:04 +0300

    fix node

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 const combineRouters = require('koa-combine-routers');
-
 const index = require('./index/index.js');
-const subscriptions = require('./subscriptions');
 
 const router = combineRouters(
   index,
-  subscriptions,
 );
 
 module.exports = router;
